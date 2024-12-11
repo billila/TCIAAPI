402eb05bf1beb60f5b46460a76ba239460607830
LiNk-NY <marcel.ramos@sph.cuny.edu>
2024-12-11 14:48:44 -0500

    move tcia_token_expires to the same file

--- R/tcia_access_token.R
+++ R/tcia_access_token.R
@@ -12,16 +12,22 @@
     resp
 }
 
+.t_diff_secs <- function(t1, t2) {
+    as.numeric(difftime(t1, t2, units = "secs"))
+}
+
 #' Set API and Obtain Access Token
 #'
 #' This function sends a request to the TCIA API to obtain an access token.
 #'
-#' @param username A character string for the username (default: "nbia_guest").
+#' @param username `character(1)` The username (default: "nbia_guest").
 #'
-#' @param client_id A character string for the client ID (default: "NBIA").
+#' @param client_id `character(1)` The client ID (default: "NBIA").
 #'
-#' @param grant_type A character string for the grant type (default:
-#'   "password").
+#' @param grant_type `character(1)` The grant type (default: "password").
+#'
+#' @param response `logical(1)` Whether to return the actual response (default:
+#'   `FALSE`).
 #'
 #' @return A character string containing the access token.
 #'
@@ -34,7 +40,8 @@
 tcia_access_token <- local({
     tokens <- new.env(parent = emptyenv())
     function(
-        username = "nbia_guest", client_id = "NBIA", grant_type = "password"
+        username = "nbia_guest", client_id = "NBIA", grant_type = "password",
+        response = FALSE
     ) {
         key <- paste(username, client_id, grant_type, sep = ":")
         now <- Sys.time()
@@ -47,7 +54,8 @@ tcia_access_token <- local({
             )
         } else {
             expires_in <-
-                tokens[[key]]$expires_in - (now - tokens[[key]]$created)
+                tokens[[key]]$expires_in -
+                    .t_diff_secs(now, tokens[[key]]$created)
             if (expires_in < 1L) {
                 if (expires_in > 0L)
                     Sys.sleep(expires_in)
@@ -58,6 +66,25 @@ tcia_access_token <- local({
                 )
             }
         }
-        tokens[[key]]$id_token
+        if (meta)
+            tokens[[key]]
+        else
+            tokens[[key]]$id_token
   }
 })
+
+#' @rdname tcia_access_token
+#'
+#' @examples
+#' tcia_token_expires()
+#' @export
+tcia_token_expires <- function(
+    username = "nbia_guest", client_id = "NBIA", grant_type = "password"
+) {
+    token <- tcia_access_token(
+        username = username, client_id = client_id, grant_type = grant_type,
+        response = TRUE
+    )
+    expires <- token$created + token$expires_in
+    message("The access token will expire on ", expires)
+}
