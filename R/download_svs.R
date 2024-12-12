25e0494ce5e1ea83828c649db6efbeb764cc7c77
LiNk-NY <marcel.ramos@sph.cuny.edu>
2024-12-12 18:22:10 -0500

    update download_svs and add svs_info

--- R/download_svs.R
+++ R/download_svs.R
@@ -1,27 +1,63 @@
 .PATHDB_TCIA_URL <- "https://pathdb.cancerimagingarchive.net"
 
-#' @param camic_id `character(1)` The CAMIC ID obtained from the 'TCIA
+#' @title Download SVS Images from the Cancer Imaging Archive
+#'
+#' @description This function downloads SVS images from the Cancer Imaging
+#'   Archive (TCIA) using the CAMIC ID obtained from the 'TCIA Histopathology
+#'   Custom Dataset Builder.json' file.
+#'
+#' @details The json file can be obtained by navigating to the TCIA website
+#'   <https://www.cancerimagingarchive.net/> under 'Access The Data' > 'Search
+#'   Histopathology Portal' and clicking on the 'TCIA Histopathology Custom
+#'   Dataset Builder' link.
+#'
+#' @param camic_ids `character()` A vector of CAMIC IDs obtained from the 'TCIA
 #'   Histopathology Custom Dataset Builder.json' file.
 #'
+#' @param destdir `character(1)` The destination directory where the SVS images
+#'   will be saved. The default is `tempdir()`.
+#'
+#' @returns * `download_svs`: The path to the downloaded file
+#'  * `svs_info`: A list containing the metadata of the SVS image
+#'
 #' @examples
-#' download_svs("311781")
+#' if (interactive()) {
+#'     svs_info("311781")
+#'     download_svs("311781")
+#'     download_svs(311781:311783)
+#' }
+#' @export
+download_svs <- function(camic_ids, destdir = tempdir()) {
+    svs_urls <- vapply(camic_ids, function(camic_id) {
+        svs_info(camic_id)$field_wsiimage[[1L]][["url"]]
+    }, character(1L))
+
+    resps <- lapply(svs_urls, request) |>
+        req_perform_parallel()
+
+    lapply(resps, .write_svs, destdir)
+}
+
+#' @rdname download_svs
+#'
+#' @param camic_id `character(1)` A single CAMIC ID.
 #'
 #' @export
-download_svs <- function(camic_id, destdir = tempdir()) {
-    resp <- request("https://pathdb.cancerimagingarchive.net/") |>
+svs_info <- function(camic_id) {
+    if (!is.character(camic_id))
+        camic_id <- as.character(camic_id)
+    request(.PATHDB_TCIA_URL) |>
         req_template("node/{camic_id}?_format=json") |>
         req_perform() |>
         resp_body_json()
-    svs_url <- resp[["field_wsiimage"]][[1L]]$url
-
-    file_content <- request(svs_url) |>
-        req_perform() |>
-        resp_body_raw()
-
-    ## TODO: check if image is already in the folder
-
-    outfile <- file.path(destdir, basename(svs_url))
-    writeBin(file_content, outfile)
-    outfile
 }
 
+.write_svs <- function(resp, destdir) {
+    bin_raw <- resp_body_raw(resp)
+    outfile <-
+        file.path(destdir, basename(resp_url(resp)))
+    if (file.exists(outfile))
+        warning("Overwriting existing file: ", outfile)
+    writeBin(bin_raw, outfile)
+    outfile
+}
